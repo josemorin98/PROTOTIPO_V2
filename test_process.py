afb6c368fa15bbb47aa0526acd8ad1e43a6f77e5
josemorin98 <josemorin9842@gmail.com>
2022-03-02 16:23:41 -0600

    names entidades

--- test_process.py
+++ test_process.py
@@ -1,10 +1,15 @@
-from enum import unique
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import datetime
+from hermetrics.metric_comparator import MetricComparator
+import json
+import numpy as np
+
+
+
 
 # df = pd.read_csv("/test/files/rezago/IRS_mpios_2000.csv")
-df = pd.read_csv("/test/prototipoTest/TasaD_pre.csv")
+df = pd.read_csv("/test/prototipoTest/TasaD_preV2.csv")
 
 # datetime.strptime(aux[x], '%Y-%m-%d %H:%M:%S')
 # print(df['anio_ocur'])
@@ -16,8 +21,12 @@ df = pd.read_csv("/test/prototipoTest/TasaD_pre.csv")
 # df.to_csv("./TasaD_preV2.csv",index=False)
 # 
 
-import json
+
  
+def name_max(vals,antenas):
+    max_val = np.argmax(vals)
+    return antenas[max_val]
+
 # Opening JSON file
 f = open('loadBalanceGeneric/states.json')
  
@@ -35,3 +44,23 @@ print(listr)
  
 # Closing file
 f.close()
+
+
+entidades = df['nombre entidad'].to_list()
+
+print(len(listr))
+
+mc = MetricComparator()
+
+newEntity = list()
+for entity in entidades:
+    aux_names = list()
+    for val in listr:
+        aux_names.append(mc.similarity(val.upper(),entity.upper())['Hamming'])
+    name_win = name_max(aux_names,listr)
+    print("{}-{}".format(entity,name_win))
+    newEntity.append(name_win)
+
+df['nombre entidad'] = newEntity
+
+df.to_csv("./TasaD_preV3.csv")
